3ae49d1858758e70ad6b50aa07e34fbad33303f6
4ce <88108711+4cecoder@users.noreply.github.com>
2023-10-12 08:02:14 +0000

    third commit

---
+++ comments.js
@@ -0,0 +1,19 @@
+// comments.js
+// create web server
+const http = require('http');
+
+function createWebServer(port) {
+  // Create an HTTP server that listens on the specified port
+  const server = http.createServer((req, res) => {
+    // Set the response header with a status code of 200 (OK)
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    // Write the "Hello, World!" message to the response
+    res.end('Hello, World!\n');
+  });
+
+  // Start the server and listen on the specified port
+  server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
